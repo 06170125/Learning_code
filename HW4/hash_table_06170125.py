308e14768eb105825fc36557a065fd0ba34f9afd
ying_chun <55686334+06170125@users.noreply.github.com>
2019-12-06 22:46:20 +0800

    Add files via upload

--- HW4/hash_table_06170125.py
+++ HW4/hash_table_06170125.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[26]:
 
 
 from Crypto.Hash import MD5
 class ListNode:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, val):
+        self.val = val
         self.next = None
     
 
 class MyHashSet:
 
-    def __init__(self, capacity):
+    def __init__(self, capacity=5):
         self.capacity = capacity
-        self.data = [None] * capacity
+        self.val = [None] * capacity
         
         
     def md5(self,key): #透過md5轉成雜湊值，所以我先設定一個md5函數
@@ -30,44 +30,46 @@ class MyHashSet:
         new = ListNode(key) #新的節點
         ind = self.md5(key) #經過md5函數的數值
         if self.contains(key) == True: #如果這個數值已經存在的話
-            return #直接回傳，因為list中同樣數值存在一次就好
+            return #直接回傳
         if self.contains(key) != True: #如果數值不存在
-            if self.data[ind] == None: #如果list沒東西，也就是加進來的東西為第一個
-                self.data[ind] = new #那他就是新的
+            if self.val[ind] == None: #如果list沒東西，也就是加進來的東西為第一個
+                self.val[ind] = new #那他就是新的
             else: #其他，如果list有東西(衝突)
-                num = self.data[ind] 
+                num = self.val[ind] 
                 while (num.next): 
                     num = num.next #跳下個，直到沒下個
                 num.next = new #就可以加了
                 
     def remove(self, key): #刪除
         ind = self.md5(key) #經過md5函數的數值
-        num = self.data[ind] 
+        num = self.val[ind] #這是節點
         none = None
-        if num and key != num.data: #如果要刪的東西根本不存在
-            none=num
-            num=num.next
         
-        if num is None: #沒數值
-            return #直接回傳
-        if none is None:
-            self.data[ind] = self.data[ind].next
+        if self.contains(key) == False: #沒數值
+            return False #直接回傳
+        #if self.contains(key) != False:
+        if num != None and key != num.val:
+            none = num
+            num = num.next
+        if none == None:
+                self.val[ind] = self.val[ind].next
         else:
             none.next = num.next
             
     def contains(self, key): #查詢
         ind=self.md5(key) #經過md5函數的數值
-        num = self.data[ind]
+        num = self.val[ind]
         if num == None: #如果查詢東西不存在
+#             print("c1")
             return False
-        if num.data == key: #如果查詢東西剛好等於第一數
+        if num.val == key: #如果查詢東西剛好等於第一數
             return True
         if num.next: #如果查詢的東西是節點
             while (num.next) != None: #下一位不適空值
                 num=num.next #往下一位繼續查
-                if num.data == key: #查到就true
+                if num.val == key: #查到就true
                     return True
-                if num.data != key: #查不到就false
+                if num.val != key: #查不到就false
                     False
         else:
             return False   
